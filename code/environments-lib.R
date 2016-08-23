c13f0dbb4e45d5196c21dca8c6cd552f01eaaa0c
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-08-23 15:06:19 -0300

    Labels for the envs

--- code/environments-lib.R
+++ code/environments-lib.R
@@ -31,6 +31,52 @@ get_environment_labels = function(){
   return(labels)
 }
 
+get_gene_labels = function(){
+  labels = structure(list(topic = 0:39, label = c("0 : Rihanna, Ellie Goulding, Katy Perry, Lana Del Rey, Imagi...", 
+                                                  "1 : Queen, Michael Jackson, The Beatles, Madonna, U2, Electr...", 
+                                                  "2 : Taylor Swift, Katy Perry, Carrie Underwood, Kelly Clarks...", 
+                                                  "3 : Radiohead, The Smiths, The National, Interpol, The Cure,...", 
+                                                  "4 : Kanye West, Kid Cudi, Drake, Jay-Z, Lil' Wayne, Lupe Fia...", 
+                                                  "5 : Linkin Park, System of a Down, Breaking Benjamin, Avenge...", 
+                                                  "6 : 소녀시대, 2NE1, SHINee, Big Bang, Super Junior, 4minute, Aft...", 
+                                                  "7 : Muse, The Killers, Placebo, Coldplay, Keane, 30 Seconds ...", 
+                                                  "8 : One Direction, Katy Perry, Britney Spears, Demi Lovato, ...", 
+                                                  "9 : Lady Gaga, Lana Del Rey, Marina & the Diamonds, Britney ...", 
+                                                  "10 : Björk, Tori Amos, Madonna, Massive Attack, PJ Harvey, T...", 
+                                                  "11 : Glee Cast, Rihanna, Lady Gaga, Taylor Swift, Katy Perry...", 
+                                                  "12 : Mariah Carey, Britney Spears, Christina Aguilera, Madon...", 
+                                                  "13 : All Time Low, Paramore, Taylor Swift, My Chemical Roman...", 
+                                                  "14 : Iron Maiden, Metallica, Megadeth, AC/DC, Porcupine Tree...", 
+                                                  "15 : Rihanna, Lady Gaga, Katy Perry, Beyoncé, Christina Agui...", 
+                                                  "16 : Coldplay, John Mayer, The Fray, Snow Patrol, Jason Mraz...", 
+                                                  "17 : Britney Spears, Wanessa, Christina Aguilera, t.A.T.u., ...", 
+                                                  "18 : 浜崎あゆみ, 倖田來未, 安室奈美恵, Boa, 宇多田ヒカル, Britney Spears, Perfum...", 
+                                                  "19 : Nightwish, Within Temptation, Evanescence, Epica, Korn,...", 
+                                                  "20 : Beyoncé, Lady Gaga, Britney Spears, Shakira, Rihanna, C...", 
+                                                  "21 : Britney Spears, Girls Aloud, The Saturdays, Cheryl Cole...", 
+                                                  "22 : Daft Punk, deadmau5, Skrillex, David Guetta, The Prodig...", 
+                                                  "23 : O.S.T.R., Pezet, The Corrs, Tori Amos, donGURALesko, El...", 
+                                                  "24 : Lana Del Rey, Florence + the Machine, Marina & the Diam...", 
+                                                  "25 : Britney Spears, Leona Lewis, Kelly Clarkson, Lady Gaga,...", 
+                                                  "26 : Arctic Monkeys, The Strokes, The Beatles, The Killers, ...", 
+                                                  "27 : Adele, Amy Winehouse, P!nk, Pink, Alanis Morissette, Al...", 
+                                                  "28 : Miley Cyrus, Taylor Swift, Demi Lovato, Britney Spears,...", 
+                                                  "29 : Eminem, Kanye West, Jay-Z, Nas, 50 Cent, 2Pac, The Game...", 
+                                                  "30 : Hey, Myslovitz, happysad, Coma, Florence + the Machine,...", 
+                                                  "31 : Rihanna, Black Eyed Peas, David Guetta, Britney Spears,...", 
+                                                  "32 : Madonna, Nelly Furtado, Alicia Keys, Wanessa, Shakira, ...", 
+                                                  "33 : Los Hermanos, Legião Urbana, The Beatles, Arctic Monkey...", 
+                                                  "34 : Rihanna, Chris Brown, Usher, Beyoncé, Nicki Minaj, Kany...", 
+                                                  "35 : Madonna, Kylie Minogue, Lady Gaga, Christina Aguilera, ...", 
+                                                  "36 : Katy Perry, Paramore, Avril Lavigne, Rihanna, The Prett...", 
+                                                  "37 : The Beatles, Pink Floyd, Nirvana, Red Hot Chili Peppers...", 
+                                                  "38 : Britney Spears, Christina Aguilera, Madonna, Rihanna, L...", 
+                                                  "39 : Hans Zimmer, Bring Me the Horizon, A Day to Remember, b..."
+  )), class = c("tbl_df", "tbl", "data.frame"), row.names = c(NA, 
+                                                              -40L), .Names = c("topic", "label"))                                                                  
+return(labels)
+}
+
 create_probabilitiesdf = function(summariesdf,
                                   label_artists = "artist",
                                   label_user = "collaborator") {
